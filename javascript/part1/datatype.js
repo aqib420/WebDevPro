0738d8c2ae8a160623a8add5d69ecc95e88be3b7
Aquib Ansari <aquibahmed2000@gmail.com>
2025-09-19 13:30:44 +0500

    js mastery started

---
+++ javascript/part1/datatype.js
@@ -0,0 +1,8 @@
+// var score = 112
+let score = 112; //New way of definign variables in Js
+let name = "Aquib Ansari";
+let isReal = true;
+
+//objects
+let teaType = ["doodh-patti", "black-tea", "green-tea", "lemon-herb"];
+let user = { firstname: "Aquib", lastname: "Ansari" };
---
+++ javascript/part1/printing.js
@@ -0,0 +1,5 @@
+console.log("Hey ");
+process.stdout.write("hellu");
+process.stdout.write("hellu");
+console.table({ city: "Karachi", name: "Aquib Ansari" });
+console.warn({ city: "Karachi" });

e96496f9ca823416376021e846f0be45bc450256
Aquib Ansari <aquibahmed2000@gmail.com>
2025-09-19 14:02:37 +0500

    part1 Js completed which includes datatypes, logcial operators and more

---
+++ javascript/part1/logical.js
@@ -0,0 +1,13 @@
+// && -> and operator
+// || -> or operator
+// ! -> reverse/not operator
+
+let isLoggedin = true;
+let isPaid = false;
+
+console.log(isLoggedin && isPaid);
+
+let isEmailuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser);
---
+++ javascript/part1/operations.js
@@ -0,0 +1,5 @@
+let num1 = 50;
+let num2 = 65;
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num2 > num1);

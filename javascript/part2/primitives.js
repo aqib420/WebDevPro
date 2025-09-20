b854fb319e9b4d897046105dafc99d1a160ebe34
Aquib Ansari <aquibahmed2000@gmail.com>
2025-09-20 23:36:06 +0500

    part2 Js primitive datatypes

---
+++ javascript/part2/primitives.js
@@ -0,0 +1,41 @@
+// Number
+let balance = 120;
+// console.log(typeof balance);
+
+// let anotherBalance = new String("nugget");
+// console.log(anotherBalance);
+// console.log(anotherBalance.valueOf());
+
+// console.log(typeof anotherBalance);
+
+//Boolean
+// let isTrue = true;
+// console.log(typeof isTrue);
+
+// let isActuallyTrue = new Boolean(true);
+// console.log(isActuallyTrue.valueOf());
+
+// console.log(typeof isActuallyTrue);
+
+// let firstName;
+// console.log(firstName);
+
+// let newName = null;
+// let oldName = undefined;
+
+//string
+// let myString = "Hello";
+// let myStringOne = "hola";
+// let username = "Aqib";
+// let oldGreet = "Hello" + username;
+// let greetMessage = `Hello + ${username}!, whatsgood?`;
+
+// console.log(oldGreet);
+// console.log(greetMessage);
+// console.log(typeof greetMessage);
+
+//Symbol-> gurantees uniqueness
+
+let sm1 = Symbol("Aquib");
+let sm2 = Symbol("Aquib");
+console.log(sm1 == sm2);

06c5f8c69ba0f4051217ba161458697d353108ea
Aquib Ansari <aquibahmed2000@gmail.com>
2025-09-24 02:01:26 +0500

    while,do-while and for loop in JS

--- javascript/part4/loopChallenges.js
+++ javascript/part4/loopChallenges.js
@@ -3,7 +3,7 @@
 */
 let sum = 0;
 let n = 1;
-while (n <= 5) {
+while (n < 6) {
   sum = sum + n;
   n++;
 }
@@ -15,7 +15,7 @@ console.log(sum);
 
 let i = 5;
 let arr1 = new Array();
-while (i != 0) {
+while (i > 0) {
   arr1.push(i);
   i--;
 }
@@ -24,4 +24,46 @@ console.log(arr1);
 3. Write a `do while` loop that prompts a user to enter their favorite tea type until they enter `"stop"`. 
    Store each tea type in an array named `teaCollection`.
 */
+// let teaCollection = new Array();
+// let tea;
+// do {
+//   tea = prompt(`Enter your favourite tea (type "stop" to finish)`);
+//   if (tea !== "stop") {
+//     teaCollection.push(tea);
+//   }
+// } while (tea !== "stop");
+// console.log(teaCollection);
 
+/* 
+4. Write a `do while` loop that adds numbers from 1 to 3 and stores the result in a variable named `total`.
+*/
+total = 0;
+i = 1;
+do {
+  total += i;
+  i++;
+} while (i < 4);
+console.log(total);
+
+/* 
+5. Write a `for` loop that multiplies each element in the array `[2, 4, 6]` by 2 and stores the results in a 
+new array named `multipliedNumbers`.
+*/
+let arr2 = [2, 4, 6];
+let multipliedNumbers = [];
+for (let i = 0; i < arr2.length; i++) {
+  multipliedNumbers.push(arr2[i] * 2);
+}
+console.log(multipliedNumbers);
+
+/* 
+6. Write a `for` loop that lists all the cities in the array 
+`["Paris", "New York", "Tokyo", "London"]` and stores each city in a new array named `cityList`.
+*/
+let cities = ["Paris", "New York", "Tokyo", "London"];
+let cityList = [];
+for (let i = 0; i < cities.length; i++) {
+  const city = cities[i];
+  cityList.push(city);
+}
+console.log(cityList);

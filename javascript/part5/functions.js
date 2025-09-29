bddff2b77a1bba0f3fdc0fd9f43c1815c29c123b
Aquib Ansari <aquibahmed2000@gmail.com>
2025-09-29 23:22:59 +0500

    nested functions, higher order functions, lexical/static scoping in Js

--- javascript/part5/functions.js
+++ javascript/part5/functions.js
@@ -40,4 +40,36 @@ as a parameter and calls it with the argument `"earl grey"`.
 Return the result of calling `makeTea`.
 */
 
-function processTeaOrder(makeTea) {}
+function makeTea(teaOrder) {
+  return `make-tea: ${teaOrder}`;
+}
+
+function processTeaOrder(teaFunc) {
+  return teaFunc("earl grey");
+}
+
+let order = processTeaOrder(makeTea);
+console.log(order);
+
+/* 
+5. Write a function named `createTeaMaker` that returns another function. 
+The returned function should take one parameter, `teaType`, and return a message like `"Making green tea"`. 
+Store the returned function in a variable named `teaMaker` and call it with `"green tea"`.
+*/
+function createTeaMaker() {
+  return function (teaType) {
+    return `Making ${teaType}`;
+  };
+}
+let teaMaker = createTeaMaker();
+console.log(teaMaker("green tea"));
+
+// function teaType(teaType) {
+//   return `Making ${teaType}`;
+// }
+
+// function createTeaMaker(returnFunc) {
+//   return returnFunc("green tea");
+// }
+// let teaMaker = createTeaMaker(teaType);
+// console.log(teaMaker);

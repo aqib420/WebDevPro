33b8446e05dca217c0f84ec1aa4a4b8767f11f97
Aquib Ansari <aquibahmed2000@gmail.com>
2025-10-29 14:54:38 +0500

    polymorphism,method overriding  encapsulation, static method,

--- javascript/part6/oop-mastery.js
+++ javascript/part6/oop-mastery.js
@@ -13,18 +13,18 @@ function Animal(type, species) {
   this.type = type;
   this.species = species;
 }
-Animal.prototype.sound = function () {
-  return `${this.species} makes a sound`;
-};
 
-Array.prototype.aquib = function () {
-  return `custom method ${this}`;
-};
-let myArr = [1, 2, 3, 4];
-console.log(myArr.aquib);
-let myNewArr = [1, 2, 3, 4, 5, 6];
+// Animal.prototype.sound = function () {
+//   return `${this.species} makes a sound`;
 
-console.log(myNewArr.aquib);
+// Array.prototype.aquib = function () {
+//   return `custom method ${this}`;
+// };
+// let myArr = [1, 2, 3, 4];
+// console.log(myArr.aquib);
+// let myNewArr = [1, 2, 3, 4, 5, 6];
+
+// console.log(myNewArr.aquib);
 
 class Vehicle {
   constructor(make, model) {
@@ -42,6 +42,75 @@ class Car extends Vehicle {
   }
 }
 
-let myCar = new Car("honda", "accord");
+let myCar = new Car(6, "autoamtic");
 console.log(myCar.drive());
 console.log(myCar.start());
+
+//Encapsulation (meaning i want to restrict the use of it to object data)
+class BankAccount {
+  constructor(parameters) {}
+  #balance = 0;
+  deposit(amount) {
+    this.#balance += amount;
+  }
+  getBalance() {
+    return `$${this.#balance}`;
+  }
+}
+
+let myAccount = new BankAccount();
+myAccount.deposit(500);
+myAccount.deposit(250);
+console.log(myAccount.getBalance());
+
+//Abstraction
+class CoffeeMachine {
+  start() {
+    //call DB
+    //filter value
+    return `starting machine...`;
+  }
+  brewCoffee() {
+    //do some complex calculations
+    return `Brewing coffee for user`;
+  }
+  pressStartButton() {
+    let msg1 = this.start();
+    let msg2 = this.brewCoffee();
+    return `${msg1} followed by, ${msg2}`;
+  }
+}
+
+let myMachine = new CoffeeMachine();
+// console.log(myMachine.brewCoffee());
+// console.log(myMachine.start());
+console.log(myMachine.pressStartButton());
+
+//Polymorphism; can take more than one form;
+class Bird {
+  fly() {
+    return `Flying...`;
+  }
+}
+
+class Penguin extends Bird {
+  fly() {
+    return `I dont fly!`;
+  }
+}
+
+let myBird = new Bird();
+let myPenguin = new Penguin();
+console.log(myBird.fly());
+console.log(myPenguin.fly());
+
+//static
+class Calculator {
+  static add(a, b) {
+    return a + b;
+  }
+}
+
+let myCalc = new Calculator();
+// console.log(myCalc.add()); //error
+console.log(Calculator.add(3, 4));
